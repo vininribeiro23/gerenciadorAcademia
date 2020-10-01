48996d3deb0a6c596619d4bd1ffbb685b84c62de
vininribeiro23 <vininribeiro@hotmail.com>
2020-10-01 20:32:31 -0300

    validando dados

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const { urlencoded } = require('express')
 const express = require ('express')
 const nunjucks = require('nunjucks')
 const routes = require('./routes')
 
 const server = express()
 
-
+server.use(express.urlencoded({extended: true }))
 server.use(express.static('public'))
 server.use(routes)
 
