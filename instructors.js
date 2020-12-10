1fe8172a84d82665ca23a0ca058b0c41acafe8af
vininribeiro23 <vininribeiro@hotmail.com>
2020-12-09 23:54:50 -0300

    listando instrutores

--- instructors.js
+++ instructors.js
@@ -3,6 +3,11 @@ const fs = require('fs')
 const data = require("./data.json")
 const { age, date } = require("./utils")
 
+//index
+
+exports.index = function (req, res){
+    return res.render('instructors/index', { instructors: data.instructors})
+}
 //show
 exports.show = function(req, res){
 
